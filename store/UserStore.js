b7ea48c0fc3586adaed7c9ee14ae0f735cc2883c
Sameera Chathuranga <sameera.chathuranga@atlaslabs.com.au>
2022-10-28 15:48:19 +0530

    register done

--- store/UserStore.js
+++ store/UserStore.js
@@ -3,28 +3,40 @@ import { userServices } from '../pages/api/serviceInitializer';
 
 class UserStore {
   response = '';
+  error = '';
   isLoading = false;
 
   constructor() {
     makeObservable(this, {
+      error: observable,
       isLoading: observable,
       response: observable,
+      setIsLoading: action,
+      setError: action,
     });
   }
   async registerUser(name, email, password) {
     try {
       this.setIsLoading(true);
+      this.setError('');
+      this.response = '';
       const response = await userServices.registerUser(name, email, password);
-      console.log(name, email, password);
       this.response = response;
     } catch (error) {
-      throw new Error(error.toString());
+      if (error.response?.status === 409) {
+        this.setError('Already have a user with this Email');
+      } else {
+        throw new Error(error.toString());
+      }
     } finally {
       this.setIsLoading(false);
     }
   }
   setIsLoading(state) {
     this.isLoading = state;
+  }
+  setError(error) {
+    this.error = error;
   }
 }
 export default UserStore;
